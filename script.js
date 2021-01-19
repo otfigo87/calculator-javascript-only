bf7a9025d4f36c8784de0531c1164ec3962b7091
otmane aatik <aaotmane@gmail.com>
2021-01-19 14:05:27 -0500

    startup

---
+++ script.js
@@ -0,0 +1,23 @@
+const display= document.querySelector("input");
+const numbers = document.querySelectorAll(".num");
+const operations = document.querySelectorAll(".operation");
+const egual = document.querySelector(".egual");
+const clear = document.querySelector(".clear");
+
+let haveDot = false;
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if(number.innerText === "." && !haveDot){
+           haveDot = true;
+        }else if(number.innerText === "." && haveDot){
+            return;
+        }
+     display.value = display.value !== "0" ? display.value +
+     number.innerText : number.innerText
+    });
+})
+
+clear.addEventListener("click", () => {
+    display.value = 0;
+})
